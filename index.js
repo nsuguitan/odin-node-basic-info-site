2513ddeb76b65b2817a861d75313d4339ebefb60
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-07-12 14:58:59 -0400

    navigate to different pages and 404 error page when url not valid

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ const hostname = "127.0.0.1";
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  const file = fs.readFileSync("index.html", "utf8");
-  console.log("Success");
-  res.end(file);
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, { "content-type": "text/html" });
+      var file = fs.readFileSync("index.html", "utf8");
+      res.end(file);
+      break;
+    case "/about":
+      res.writeHead(200, { "content-type": "text/html" });
+      var file = fs.readFileSync("about.html", "utf8");
+      res.end(file);
+      break;
+    case "/contact-me":
+      res.writeHead(200, { "content-type": "text/html" });
+      var file = fs.readFileSync("contact-me.html", "utf8");
+      res.end(file);
+      break;
+    default:
+      res.writeHead(200, { "content-type": "text/html" });
+      var file = fs.readFileSync("404.html", "utf8");
+      res.end(file);
+  }
 });
 
 server.listen(port, () => {
